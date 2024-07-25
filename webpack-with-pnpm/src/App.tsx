84ec6d515e47d5ef21a35120d39238f0acadc138
devkyoung2 <devkyoung2@gmail.com>
2024-07-25 17:22:45 +0900

    pnpm 으로 웹팩 dev-server 구축하기

---
+++ webpack-with-pnpm/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <>
+    <div>헤더</div>
+    <div className="text-3xl font-bold underline">Test</div>
+    <div>푸터</div>
+  </>
+);
+
+export default App;
---
+++ webpack-with-pnpm/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PORT = 3000;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    host: 'localhost',
+    port: PORT,
+    open: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.ts$/, //시험 속성
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
